9c14d48ca8f6a506212cd67864bdb93d2223ac23
louiedweck <dweck.louis@gmail.com>
2019-11-12 18:58:57 -0800

    quiz class

---
+++ app.py
@@ -0,0 +1,11 @@
+def valid_user_input(prompt, options):
+    '''Ensures user input is within valid options. '''
+    user_input = input(prompt)
+    while user_input not in options:
+        print("Invalid option")
+        user_input = input(prompt)
+    return user_input
+
+
+class quiz_questions(valid_user_input):
+    pass
